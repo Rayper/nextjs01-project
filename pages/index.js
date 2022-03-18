cf4c73901c6687d7690f3b81228e7ef6d4776c9e
Rayper <raihangunawan22@gmail.com>
2022-03-18 14:05:46 +0700

    video 3.3 DIsplaying Events

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,26 @@
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import Layout from  '@/components/Layout'
+import EventItem from  '@/components/EventItem'
 import { API_URL } from '@/config/index'
+import Link from 'next/link'
 
-export default function Home({events}) {
-console.log(events);
-
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
@@ -18,10 +30,9 @@ export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`)
   const events = await res.json()
 
-  console.log(events);
-
   return {
-    props: {events},
+    // nampilin 3 events aja
+    props: { events:events.slice(0 , 3) },
     // 1 second delay untuk mendapatkan data yang terupdate
     revalidate: 1
   }
