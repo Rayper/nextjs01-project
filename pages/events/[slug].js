967537fd363435b7b593a4e48f0ff89d2a797af6
Rayper <raihangunawan22@gmail.com>
2022-03-18 09:23:52 +0700

    video 2.3 Head Tags and Layout

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,5 +1,6 @@
 import React from 'react'
 // import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
 
 export default function EventPage() {
   // const router = useRouter()
@@ -7,9 +8,9 @@ export default function EventPage() {
   // console.log(router);
 
   return (
-    <div>
+    <Layout>
         <h1>My Events</h1>
         <h3>{router.query.slug}</h3>
-    </div>
+    </Layout>
   )
 }
