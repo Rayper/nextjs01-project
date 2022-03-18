15e4766074eb621d6bb710937569c7b2142045b2
Rayper <raihangunawan22@gmail.com>
2022-03-18 09:54:29 +0700

    video 2.6 404 Pages

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+import Link from 'next/link'
+import { FaExclamationTriangle } from 'react-icons/fa'
+ 
+export default function NotFoundPage() {
+  return (
+    <Layout title={'Page Not Found'}>
+        <div className={styles.error}>
+            <h1> <FaExclamationTriangle /> 404 Not Found</h1>
+            <h4>Sorry, there is nothing here</h4>
+            <Link href='/'>Go Back</Link>
+        </div>
+    </Layout>
+  )
+}
