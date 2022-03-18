cc5c3f9acd0bd51dbe0f20f1cc2a59ab64656690
Rayper <raihangunawan22@gmail.com>
2022-03-18 08:50:06 +0700

    video 2.3 useRouter

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,15 @@
 import React from 'react'
+// import { useRouter } from 'next/router'
 
 export default function EventPage() {
+  // const router = useRouter()
+
+  // console.log(router);
+
   return (
     <div>
         <h1>My Events</h1>
+        <h3>{router.query.slug}</h3>
     </div>
   )
 }
