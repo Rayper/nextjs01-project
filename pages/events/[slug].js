54feba65ce42e89c568ebccc9d1a203cd3de2da1
Rayper <raihangunawan22@gmail.com>
2022-03-18 15:06:17 +0700

    video 3.4 getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,16 +1,59 @@
 import React from 'react'
 // import { useRouter } from 'next/router'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index';
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   // const router = useRouter()
-
   // console.log(router);
-
   return (
     <Layout>
-        <h1>My Events</h1>
-        {/* <h3>{router.query.slug}</h3> */}
+        {/* <h1>{evt.name}</h1> */}
     </Layout>
   )
 }
+
+export async function getStaticPaths() {
+  // create semua paths dengan slug
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map((evt) => ({
+    params: {slug: evt.slug}
+  }))
+
+  return {
+    paths,
+    // set to false akan return 404 ketika resource dari slug tidak ditemukan ataupun path tidak ditemukan
+    fallback: true,
+  }
+}
+
+// pakai parameter params dari params yang dikirimkan oleh variable paths
+export async function getStaticProps({ params: {slug} }) {
+    console.log(slug);
+
+    const res = await fetch(`${API_URL}/api/events/${slug}`)
+    const events = await res.json()
+
+    return {
+      props: {
+        evt: events[0],
+      },
+      revalidate: 1
+    }
+}
+
+// export async function getServerSideProps({ params: {slug} }) {
+//   console.log(slug);
+
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//     revalidate: 1
+//   }
+// }
