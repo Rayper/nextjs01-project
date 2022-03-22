cce04009555e85ecd4bd7d7f4aa6c95f16636f7b
Rayper <raihangunawan22@gmail.com>
2022-03-22 11:08:20 +0700

    video 7.4 Login & get JWT

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect, createContext } from 'react'
-import { API_URL } from '../config/index'
+import { NEXT_URL } from '../config/index'
 import { useRouter } from 'next/router'
 
 const AuthContext = createContext()
@@ -17,7 +17,31 @@ export const AuthProvider = ({children}) => {
 
     // Login User
     const login = async ({email:identifier, password}) => {
-        console.log({identifier, password});
+        const res = await fetch(`${NEXT_URL}/api/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:  JSON.stringify({
+                identifier,
+                password
+            })
+        })
+
+        const data = await res.json()
+
+        console.log(data)
+
+        if(res.ok) {
+            // set user dari data yang dikirim
+            setUser(data.user)
+
+        } else {
+            // set Error dari data yang dikirim dari login.js
+            setError(data.message)
+            // supaya ga stuck di state
+            setError(null)
+        }
     }
 
     // Logout User
---
+++ pages/api/login.js
@@ -0,0 +1,31 @@
+import { API_URL } from "@/config/index";
+
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        const { identifier, password } = req.body;
+        const strapiRes = await fetch(`${API_URL}/auth/local`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ identifier, password }),
+        });
+
+        const data = await strapiRes.json();
+
+        // nampilin jwt pada log
+        console.log(data.jwt);
+    
+        if (strapiRes.ok) {
+            // set ke cookie
+            res.status(200).json({ user: data.user });
+        } else {
+            res
+            .status(data.statusCode)
+            .json({ message: data.message[0].messages[0].message });
+        }
+        } else {
+            res.setHeader('');
+            req.status(405).json({ message: `Method ${req.method} not allowed` });
+        }
+}
